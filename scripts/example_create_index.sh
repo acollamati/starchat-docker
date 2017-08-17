09a7b7e7437cca1235f737542d04f13c23ad048b
Angelo Leto <angleto@gmail.com>
2017-08-17 15:52:25 +0200

    fixed typo

--- scripts/example_create_index.sh
+++ scripts/example_create_index.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -v -H "Content-Type: application/json" -X POST "http://localhost:8888/index_management/create"^C
+curl -v -H "Content-Type: application/json" -X POST "http://localhost:8888/index_management/create"
 
