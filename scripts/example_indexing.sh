d7e6b515d47bb1b2e51be99e299b90f4b4257371
Angelo Leto <angleto@gmail.com>
2017-08-17 16:16:04 +0200

    improved example_indexing.sh script

--- scripts/example_indexing.sh
+++ scripts/example_indexing.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -i -t --rm --name starchat_cmd -v /tmp/input_data:/input_data:ro elegansio/starchat:latest /starchat/bin/index-decision-table --inputfile /input_data/decision_table_starchat_doc.csv --host http://192.168.1.111:8888
+HOST_IP="192.168.11.111"
+docker run -i -t --rm --name starchat_cmd -v /tmp/input_data:/input_data:ro elegansio/starchat:latest /starchat/bin/index-decision-table --inputfile /input_data/decision_table_starchat_doc.csv --host http://${HOST_IP}:8888
 
