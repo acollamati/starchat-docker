84c8a41d414ef04e0d2d88562e892971b2326f66
Angelo Leto <angleto@gmail.com>
2017-12-19 16:32:59 +0100

    update starchat version

--- scripts/example_indexing.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-HOST_IP="192.168.11.111"
-docker run -i -t --rm --name starchat_cmd -v /tmp/input_data:/input_data:ro elegansio/starchat:latest /starchat/bin/index-decision-table --inputfile /input_data/decision_table_starchat_doc.csv --host http://${HOST_IP}:8888
-
